742ec4076e87ace5de087f67c08505379bba0192
Nahemah Chen <a122774007@gmail.com>
2023-11-11 18:00:23 -0500

    cicd: lambda deploy

--- src/lambda/lf1/lambda_function.py
+++ src/lambda/lf1/lambda_function.py
@@ -1,12 +1,52 @@
 import json
+import requests
+import boto3
+
+import opensearch_client
+from datetime import datetime
 
 def lambda_handler(event, context):
     awsRegion = event['Records'][0]['awsRegion']
     bucket = event['Records'][0]['s3']['bucket']['name']
-    path = event['Records'][0]['s3']['object']['key']
-    IMAGE_URL = f"https://{bucket}.s3.{awsRegion}.amazonaws.com/{path}"
+    key = event['Records'][0]['s3']['object']['key']
+    IMAGE_URL = f"https://{bucket}.s3.{awsRegion}.amazonaws.com/{key}"
     print(IMAGE_URL)
+    labels = [label['Name'] for label in recognize_labels(bucket, key)["Labels"]]
+    # print(labels)
+    # index_photo(bucket, key, IMAGE_URL, labels)
+    
+    # res = opensearch_client.query('Nature', 1)
+    # print(res)
+    
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
+    
+def index_photo(bucket, key, image_url, labels):
+    document = {
+        "objectKey": key,
+        "bucket": bucket,
+        "createdTimestamp": datetime.now().isoformat(),
+        "labels": labels
+    }
+    print(document)
+    response = opensearch_client.insert(document, image_url)
+    print(response)
+        
+def recognize_labels(bucket, key):
+    client = boto3.client("rekognition")
+    s3 = boto3.client("s3")
+    fileObject = s3.get_object(Bucket=bucket, Key=key)
+    file_content = fileObject["Body"].read()
+    response = client.detect_labels(
+        Image={
+            'S3Object': {
+                'Bucket': bucket,
+                'Name': key,
+            },
+        },
+        MaxLabels=123,
+        MinConfidence=70,
+    )
+    return response
---
+++ src/lambda/lf1/opensearch_client.py
@@ -0,0 +1,58 @@
+import json
+import os
+
+import boto3
+from opensearchpy import OpenSearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+
+REGION = 'us-east-2'
+HOST = 'search-photos-szqe434hz3ystqqh2xqr5umx4a.us-east-2.es.amazonaws.com'
+INDEX = 'photos'
+
+def query(term, num):
+    q = {'size': num, 'query': {'multi_match': {'query': term}}}
+
+    client = OpenSearch(hosts=[{
+        'host': HOST,
+        'port': 443
+    }],
+    http_auth=get_awsauth(REGION, 'es'),
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection)
+
+    res = client.search(index=INDEX, body=q)
+
+    hits = res['hits']['hits']
+    results = []
+    for hit in hits:
+        results.append(hit['_source'])
+
+    return results
+
+
+def get_awsauth(region, service):
+    cred = boto3.Session().get_credentials()
+    return AWS4Auth(cred.access_key,
+                    cred.secret_key,
+                    region,
+                    service,
+                    session_token=cred.token)
+
+def insert(document, id):
+    client = OpenSearch(hosts=[{
+        'host': HOST,
+        'port': 443
+    }],
+    http_auth=get_awsauth(REGION, 'es'),
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection)
+
+    response = client.index(
+        index = INDEX,
+        body = document,
+        id = id,
+        refresh = True
+    )
+    return response

8da53749c36af31b4fa7ca0ab6ca3f7aa7bce831
Nahemah Chen <a122774007@gmail.com>
2023-11-12 09:16:56 -0500

    lf1: apply metadata for labeling

--- src/lambda/lf1/lambda_function.py
+++ src/lambda/lf1/lambda_function.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import boto3
+import base64
 
 import opensearch_client
 from datetime import datetime
@@ -11,11 +12,10 @@ def lambda_handler(event, context):
     key = event['Records'][0]['s3']['object']['key']
     IMAGE_URL = f"https://{bucket}.s3.{awsRegion}.amazonaws.com/{key}"
     print(IMAGE_URL)
-    labels = [label['Name'] for label in recognize_labels(bucket, key)["Labels"]]
-    # print(labels)
-    # index_photo(bucket, key, IMAGE_URL, labels)
+    labels = recognize_labels(bucket, key)
+    index_photo(bucket, key, IMAGE_URL, labels)
     
-    # res = opensearch_client.query('Nature', 1)
+    # res = opensearch_client.query('Capoo', 1)
     # print(res)
     
     return {
@@ -31,22 +31,26 @@ def index_photo(bucket, key, image_url, labels):
         "labels": labels
     }
     print(document)
-    response = opensearch_client.insert(document, image_url)
-    print(response)
+    opensearch_client.insert(document, image_url)
         
 def recognize_labels(bucket, key):
     client = boto3.client("rekognition")
     s3 = boto3.client("s3")
+    meta = s3.head_object(
+        Bucket=bucket,
+        Key=key,
+    )
+    print(meta)
     fileObject = s3.get_object(Bucket=bucket, Key=key)
     file_content = fileObject["Body"].read()
-    response = client.detect_labels(
+    bytes = base64.b64decode(file_content)
+    detected_labels = client.detect_labels(
         Image={
-            'S3Object': {
-                'Bucket': bucket,
-                'Name': key,
-            },
+            'Bytes': bytes
         },
         MaxLabels=123,
         MinConfidence=70,
     )
-    return response
+    res = [label['Name'] for label in detected_labels["Labels"]]
+    res.extend(meta['Metadata']['customlabels'].split(','))
+    return res
